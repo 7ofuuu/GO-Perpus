8c241d75ac7cee58ce03d0f5bf8cedae7522cfbd
Fadhli Rabbi <fadhlirabbi46@gmail.com>
2024-05-19 21:02:38 +0700

    last sebelum laporan alpro

--- ProgramPerpustakaan.go
+++ ProgramPerpustakaan.go
@@ -6,17 +6,24 @@ import (
 	"os"
 	"sort"
 	"strings"
+	"time"
 
 	"github.com/olekukonko/tablewriter"
 )
 
 // Book struct represents the details of a book
 type Book struct {
-	Title  string
-	Author string
-	ISBN   string
-	Copies int
-	// Add other book details as needed
+	Title     string
+	Author    string
+	ISBN      string
+	Copies    int
+	Borrowers []Borrower // To keep track of who borrowed the book and the due date
+}
+
+// Borrower struct represents a borrower with name and due date
+type Borrower struct {
+	Name    string
+	DueDate time.Time
 }
 
 // Library struct represents the library with its collection of books
@@ -58,6 +65,79 @@ func (lib *Library) RemoveBook(isbn string) {
 			break
 		}
 	}
+}
+
+// BorrowBook borrows a book from the library
+func (lib *Library) BorrowBook(isbn string, borrower string) {
+	for i, book := range lib.Books {
+		if book.ISBN == isbn {
+			if book.Copies > 0 {
+				if lib.CanBorrowMoreBooks(borrower) {
+					if !lib.HasBorrowedSameBook(isbn, borrower) {
+						dueDate := time.Now().AddDate(0, 0, 7)
+						lib.Books[i].Copies--
+						lib.Books[i].Borrowers = append(lib.Books[i].Borrowers, Borrower{Name: borrower, DueDate: dueDate})
+						fmt.Printf("Book borrowed successfully by %s. Due date: %s\n", borrower, dueDate.Format("02-Jan-2006"))
+					} else {
+						fmt.Println("You have already borrowed this book.")
+					}
+				} else {
+					fmt.Println("You cannot borrow more than 3 books.")
+				}
+			} else {
+				fmt.Println("No copies available for borrowing.")
+			}
+			return
+		}
+	}
+	fmt.Println("Book not found.")
+}
+
+// ReturnBook returns a book to the library
+func (lib *Library) ReturnBook(isbn string, borrower string) {
+	for i, book := range lib.Books {
+		if book.ISBN == isbn {
+			for j, b := range book.Borrowers {
+				if b.Name == borrower {
+					lib.Books[i].Copies++
+					lib.Books[i].Borrowers = append(lib.Books[i].Borrowers[:j], lib.Books[i].Borrowers[j+1:]...)
+					fmt.Printf("Book returned successfully by %s\n", borrower)
+					return
+				}
+			}
+		}
+	}
+	fmt.Println("Book was not borrowed by this user.")
+}
+
+// CanBorrowMoreBooks checks if the user has borrowed less than 3 books
+func (lib *Library) CanBorrowMoreBooks(borrower string) bool {
+	count := 0
+	for _, book := range lib.Books {
+		for _, b := range book.Borrowers {
+			if b.Name == borrower {
+				count++
+				if count >= 3 {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+// HasBorrowedSameBook checks if the user has already borrowed the same book
+func (lib *Library) HasBorrowedSameBook(isbn string, borrower string) bool {
+	for _, book := range lib.Books {
+		if book.ISBN == isbn {
+			for _, b := range book.Borrowers {
+				if b.Name == borrower {
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
 
 // SortBooks sorts the library's books by title
@@ -130,24 +210,6 @@ func main() {
 		fmt.Println("Error loading library data:", err)
 	}
 
-	// Add some sample books if the library is empty
-	if len(library.Books) == 0 {
-		for i := 0; i < 3; i++ {
-			fmt.Println("Enter details for book", i+1)
-			title := getInput("Title:")
-			author := getInput("Author:")
-			isbn := getInput("ISBN:")
-			copies := getInput("Number of copies:")
-			var copiesInt int
-			fmt.Sscanf(copies, "%d", &copiesInt)
-			library.AddBook(Book{Title: title, Author: author, ISBN: isbn, Copies: copiesInt})
-		}
-		// Save library data to file after adding sample books
-		if err := library.SaveLibrary("library.txt"); err != nil {
-			fmt.Println("Error saving library data:", err)
-		}
-	}
-
 	for {
 		fmt.Println("\nOptions:")
 		fmt.Println("1. Add Book")
@@ -155,7 +217,9 @@ func main() {
 		fmt.Println("3. Edit Book")
 		fmt.Println("4. Search Books")
 		fmt.Println("5. Display All Books")
-		fmt.Println("6. Exit")
+		fmt.Println("6. Borrow Book")
+		fmt.Println("7. Return Book")
+		fmt.Println("8. Exit")
 
 		choice := getInput("Enter your choice:")
 
@@ -205,10 +269,28 @@ func main() {
 			fmt.Println("All Books:")
 			displayBooks(library.Books)
 		case "6":
+			fmt.Println("Books available for borrowing:")
+			displayBooks(library.Books)
+			isbn := getInput("Enter ISBN of the book to borrow:")
+			borrower := getInput("Enter your name:")
+			library.BorrowBook(isbn, borrower)
+			// Save library data to file after borrowing a book
+			if err := library.SaveLibrary("library.txt"); err != nil {
+				fmt.Println("Error saving library data:", err)
+			}
+		case "7":
+			isbn := getInput("Enter ISBN of the book to return:")
+			borrower := getInput("Enter your name:")
+			library.ReturnBook(isbn, borrower)
+			// Save library data to file after returning a book
+			if err := library.SaveLibrary("library.txt"); err != nil {
+				fmt.Println("Error saving library data:", err)
+			}
+		case "8":
 			fmt.Println("Exiting...")
 			return
 		default:
-			fmt.Println("Invalid choice. Please enter a number between 1 and 6.")
+			fmt.Println("Invalid choice. Please enter a number between 1 and 8.")
 		}
 	}
 }
@@ -216,9 +298,14 @@ func main() {
 // displayBooks displays a list of books in a table format
 func displayBooks(books []Book) {
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Title", "Author", "ISBN", "Copies"})
+	table.SetHeader([]string{"Title", "Author", "ISBN", "Copies", "Borrowers"})
 	for _, book := range books {
-		table.Append([]string{book.Title, book.Author, book.ISBN, fmt.Sprintf("%d", book.Copies)})
+		var borrowerDetails []string
+		for _, borrower := range book.Borrowers {
+			borrowerDetails = append(borrowerDetails, fmt.Sprintf("%s (Due: %s)", borrower.Name, borrower.DueDate.Format("02-Jan-2006")))
+		}
+		borrowers := strings.Join(borrowerDetails, ", ")
+		table.Append([]string{book.Title, book.Author, book.ISBN, fmt.Sprintf("%d", book.Copies), borrowers})
 	}
 	table.Render()
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.22.2
 
 require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // direct
 	github.com/rivo/uniseg v0.4.7 // indirect
 )
